211229224ac0c276cecd617680c5fc50cf8c52d1
Mahapatra <rishav.mahapatra@contractor.axaxl.com>
2023-04-02 01:49:44 +0530

    About section added

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ function Home(){
     return(
         <div className={classes.home}>
             <div className={classes.text}>
-            <p>HI,<br></br>I am Rishav Mahapatra.</p>
+            <p>Hi,<br></br>I am Rishav Mahapatra.</p>
             <p className={classes.subtext}>I'm a Web Developer.</p>
             </div>
             
